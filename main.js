066d2ce57e44564ffb87d98bd9acfde976207d3d
hapo-linhptm <linh.ptm@haposoft.com>
2023-09-23 21:06:53 +0700

    lesson3: mouse-event/over, leave, dblclick, move

--- main.js
+++ main.js
@@ -1,24 +1,21 @@
 const app = Vue.createApp({
-    template: `
-        <h2>nội dung biến {{ tenBien }}</h2> 
-        <button v-on:click="onChangeName"> change </button>
-        <button @click="onChangeName"> show modal </button>
-        <div class="modal" v-if="isShowModal">
-            <h2>this is modal</h2>
-        </div>
-    `,
+    template:``,
     data() {
         return {
-            tenBien: "XXX",
-            tenBien2: "val: vue bien abc2",
-            isShowModal: false,
+            x: 0,
+            y: 0,
         }
     },
     methods: {
-        onChangeName() {
-            this.tenBien = 'ten bien da change';
-            this.isShowModal = !this.isShowModal;
-        }
+        onLogEvent(e, xDefautl, yDefautl) {
+            console.log(e);
+            this.x = xDefautl;
+            this.y = yDefautl;
+        },
+        onMouseMoveEvent(e) {
+            this.x = e.offsetX;
+            this.y = e.offsetY;
+        },
     },
 });
 
